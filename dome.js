a46c47eeb8cb2d17f8a39775e0c628516c944229
Roman Gurovich <romangurovich@gmail.com>
2014-01-16 00:02:17 -0800

    Add ability to set and get DOM element text and html

--- dome.js
+++ dome.js
@@ -16,6 +16,40 @@ window.dome = (function () {
     return results;
   };
 
+  Dome.prototype.forEach = function (callback) {
+    this.map(callback);
+    return this;
+  };
+
+  Dome.prototype.mapOne = function (callback) {
+    var m = this.map(callback);
+    return m.length > 1 ? m : [0];
+  }
+
+  Dome.prototype.text = function (text) {
+    if (typeof text !== "undefined") {
+      return this.forEach(function (el) {
+        el.innerText = text;
+      });
+    } else {
+      return this.mapOne(function (el) {
+        return el.innerText;
+      });
+    }
+  };
+
+  Dome.prototype.html = funciton (html) {
+    if (typeof html !== "undefined") {
+      return this.forEach(function (el) {
+        el.innerHTML = html;
+      });
+    } else {
+      return this.mapOne(function (el) {
+        return el.innerHTML;
+      });
+    }
+  };
+
   var dome = {
     get: function (selector) {
       var els;
